b141f25da94cf4d2af1dce8cf28969eb90d5ecad
putrarohaizad <putrarohayzad@gmail.com>
2020-07-29 20:32:24 +0800

    scaffold project

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,31 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Search v-bind:maklumats="details"/>
   </div>
 </template>
-
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Search from './components/Search'
+import './assets/styles/app.css';
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Search
+  },
+
+  data(){
+    return {
+      details:[{
+      id: 1,
+      name: "Putra",
+      age:24
+      }
+      ]
+    }
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
---
+++ src/components/Search.vue
@@ -0,0 +1,22 @@
+<template>
+<div>
+    <div class="container text-red-300">
+        <div class="text-red-300" v-bind:key="maklumat.id" v-for="maklumat in maklumats">
+            <h1>{{maklumat.name}}</h1>
+        </div>
+    </div>
+    
+</div>
+
+</template>
+
+<script>
+export default {
+    name : "Search",
+    props: ["maklumats"]
+}
+</script>
+
+<style scoped>
+
+</style>
